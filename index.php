5e0b57f4e9ed41fe70cfe0566058981f7bb394bc
Gooman-rus <puklo24@gmail.com>
2013-01-13 11:04:49 +0200

    rtr

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <!DOCTYPE HTML PUBLIC "-//W3C?//DTD HTML 4.01 TRANSITIONAL//EN">
-<!-- teffst -->
 <html>
 	<head>
-		<title>My homepage</title>
+		<title>About me <- My homepage</title>
 		
 		<link rel="icon" href="/favicon.ico" type="image/x-icon">
 		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 		<meta http-EQUIV="Content-Type" Content="text/html; charset=windows-1251">
 		<link href="style.css" TYPE="text/css" rel="stylesheet">
+		
+		<script language = "JavaScript">
+			var bigsize = "300";   // размер большой картинки
+			var smallsize = "150"; // размер маленькой картинки
+			function ChangeImageSize(im) 
+			{
+				if (im.height == bigsize) 
+					im.height = smallsize;
+				else 
+					im.height = bigsize;
+			}
+		</script>
+		
 	</head>
 
 	<body>
@@ -16,9 +28,9 @@
 				<td colspan=2 width=100% height=153 bgcolor="#806b2a">
 					<table width=100% CELLPADDING=0 CELLSPACING=0>
 						<tr>
-						<td><img src="images/zombie.png" alt="пїЅпїЅпїЅпїЅпїЅ"></td>
-						<td><img src="images/text.png" alt="пїЅпїЅпїЅпїЅпїЅ"></td>
-						<td align=right><img src="images/author.png" alt="пїЅпїЅпїЅпїЅпїЅ"></td>
+						<td><img src="images/zombie.png" alt="Зомби"></td>
+						<td><img src="images/text.png" alt="Текст"></td>
+						<td align=right><img src="images/author.png" alt="Me"></td>
 						</TR>
 					</table>
 				
@@ -33,19 +45,18 @@
 				<td width=25% height=100% bgcolor="#f5f5dc">
 					<div id="menu1">
                         <ul>
-							<li><a href="index.php"><b>->Main page<-</b></a></li>
-							<li><a href="about.php">About me</a></li>
+							<li><a href="index.php">Main page</a></li>
+							<li><a href="about.php"><b>->About me<-</b></a></li>
 							<li><a href="interests.php">My interests</a></li>
 							<li><a href="photos.php">Photos</a></li>
 							<li><a href="test.php">Test</a></li>
                         </ul>
                 </div> </td>
-			
-				<td width=100% height=100% bgcolor="#f5f5dc">РџСЂРёРІРµС‚. Р­С‚Рѕ РјРѕСЏ РґРѕРјР°С€РЅСЏСЏ СЃС‚СЂР°РЅРёС†Р°. РњРµРЅСЏ Р·РѕРІСѓС‚ РЈРІРёРЅ Р®СЂРёР№ Р’РёРєС‚РѕСЂРѕРІРёС‡. РЇ РёР· РіСЂСѓРїРїС‹ Р-31.
-				
+				<td width=100% height=100% bgcolor="#f5f5dc">Привет. Это моя домашняя страница. Меня зовут Увин Юрий Викторович. Я из группы И-31.
+					
 				</td>
 			</tr>
-			
+
 			<tr>
 				<td colspan=2 width=100% height=40 background="images/border1.jpg"> </td>
 			</tr>
